824f417552af0eb965a4f9251a4c26ce1c014372
shashank-shark <shashankreddy231@gmail.com>
2020-01-05 19:33:49 +0530

    different states in android.

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /* calling onCreate() method */
+        Log.d("TYPE", "calling onCreate() method");
+
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        /* calling onStart() method */
+        Log.d("TEST", "calling onStart() method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        /* calling onStop() method */
+        Log.d("TEST", "calling onStop() method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /* calling onResume() method */
+        Log.d("TYPE", "calling onResume() method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        /* calling onPause() method */
+        Log.d("TEST", "calling onPause() method");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        /* calling onDestroy() method */
+        Log.d("TEST", "calling onDestroy() method");
+    }
+
 }

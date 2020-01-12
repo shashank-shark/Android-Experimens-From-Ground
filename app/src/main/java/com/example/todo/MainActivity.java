8bb8e937fc21bb9bd9ee4e60d03555591198b474
shashank-shark <shashankreddy231@gmail.com>
2020-01-12 20:06:34 +0530

    support for kannada langage added - locale.

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -16,44 +16,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        /* calling onStart() method */
-        Log.d("TEST", "calling onStart() method");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        /* calling onStop() method */
-        Log.d("TEST", "calling onStop() method");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        /* calling onResume() method */
-        Log.d("TYPE", "calling onResume() method");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        /* calling onPause() method */
-        Log.d("TEST", "calling onPause() method");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        /* calling onDestroy() method */
-        Log.d("TEST", "calling onDestroy() method");
-    }
-
 }
